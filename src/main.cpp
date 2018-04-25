46ad965406b8bfda443cff7ba637596a3b7297e6
TinyCoin2018 <38718581+TinyCoin2018@users.noreply.github.com>
2018-04-25 15:55:51 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1001,7 +1001,7 @@ uint256 WantedByOrphan(const CBlock* pblockOrphan)
 int64_t GetProofOfWorkReward(int64_t nFees)
 {
     
-            int64_t nSubsidy = 500 * COIN;
+            int64_t nSubsidy = 1 * COIN;
 
             if(nBestHeight == 0)
             {
